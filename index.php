ed30e6ad6708725ed3fa70a0c2a42aeca74b9627
zhenghh <zhenghh@2345.com>
2019-01-16 13:31:25 +0800

    修改

--- index.php
+++ index.php
@@ -24,39 +24,75 @@ $all = $arr->all();
 $links = $ql->setHtml($html)->find("#mywztag li a")->attrs('href');
 $links = $links->all();
 
+// 拼接数组，排序
 $data = array_combine($all, $links);
 ksort($data, SORT_NATURAL);
 
-// 按照类别来分组，拼接数组
-$result = [];
-foreach ($data as $k => $v) {
-    if (strpos($k, '_') !== false) {
-        $name = substr($k, 0, strpos($k, '_'));
+// 按照类别来分组
+$info = [];
+foreach ($data as $tag => $link) {
+    $posLeftBrackets = stripos($tag, '(');
+
+    // 共有多少个收藏
+    $num = substr($tag, $posLeftBrackets + 1, -1);
+
+    // tag进行分类
+    if (stripos($tag, '_') !== false) {
+        // a_b_c(10)
+        $category = substr($tag, 0, strpos($tag, '_'));
+        $subCategory = substr($tag, 0, stripos($tag, '_'));
     } else {
-        $name = substr($k, 0, strpos($k, '('));
+        // a(10)
+        $subCategory = $category = substr($tag, 0, $posLeftBrackets);
     }
-    $result[$name][$k] = $v;
+
+
+    $list[$subCategory][$tag] = [
+        'link' => $link,
+        'num' => $num,
+    ];
+    $info[$category] = [
+        'list' => $list,
+        'num' => $num,
+    ];
 }
 
 
 
+/**
+ * 页面显示
+ */
+$loader = new Twig_Loader_Filesystem(__DIR__);
+$twig = new Twig_Environment($loader, [
+    'cache' => __DIR__ . '/twig_cache/',
+]);
+
+echo $twig->render('index.html', ['info' => $info]);
 
 
 
 
-$str = '';
-$i = 1;
 
-foreach ($result as $category => $all) {
-    $str .= '<br><h3>' . $category . '</h3>';
-    $str .= '<ul>';
-    foreach ($all as $k => $v) {
-        $str .=
-            '<li>
-              <a target="_blank" href="' . $v . '">' . $k . '</a>
-            </li>';
-    }
-    $str .= '</ul>';
-}
 
-echo str_replace("{str}", $str, file_get_contents("index.html"));
+
+
+
+// $str = '';
+// $i = 1;
+//
+// foreach ($result as $category => $all) {
+//     // 大类别
+//     $str .= '<br><h3>' . $category . '</h3>';
+//
+//     // 子类别
+//     $str .= '<ul>';
+//     foreach ($all as $k => $v) {
+//         $str .=
+//             '<li>
+//               <a target="_blank" href="' . $v . '">' . $k . '</a>
+//             </li>';
+//     }
+//     $str .= '</ul>';
+// }
+//
+// echo str_replace("{str}", $str, file_get_contents("index.html"));
--- update.php
+++ update.php
@@ -3,9 +3,34 @@
  * 处理上传
  */
 
-$path = $_FILES['tags']['tmp_name'];
-$content = file_get_contents($path);
+if (PHP_SESSION_ACTIVE != session_status()) {
+    session_start();
+}
 
-file_put_contents(sys_get_temp_dir() . '/bookmark.html', $content);
+if ('post' == strtolower($_SERVER['REQUEST_METHOD'])) {
+    if ($_SESSION['token'] != $_POST['token'] ||
+        $_FILES['tags']['error'] != 0 ||
+        $_FILES['tags']['type'] != 'text/html') {
+        echo '无效提交';
+        exit();
+    }
 
-header("location:index.php");
+    $file = new SplFileInfo($_FILES['tags']['tmp_name']);
+    if (!$file->isFile() || $file->getExtension() != 'html') {
+        echo '无效提交';
+        exit();
+    }
+
+    $path = $_FILES['tags']['tmp_name'];
+    $content = file_get_contents($path);
+
+    file_put_contents(sys_get_temp_dir() . '/bookmark.html', $content);
+
+    header("location:index.php");
+}
+
+$randomToken = uniqid();
+$_SESSION['token'] = $randomToken;
+
+$str = file_get_contents('update.html');
+echo str_replace('{{token}}', $randomToken, $str);
